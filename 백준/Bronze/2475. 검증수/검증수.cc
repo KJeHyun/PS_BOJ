e0fe714199085667421ac0cc553bea499d4c7744
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-05-10 01:35:43 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+    fast_io
+        int num = 0, a;
+    for (int i = 0; i < 5; i++) {
+        cin >> a;
+            num += a * a;
+        }
+    cout << num % 10;
+}
