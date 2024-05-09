5faa19836d8ffc29842f08342d86b4711c956674
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-05-10 01:30:04 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int A, B;
+    cin >> A >> B;
+    if(A < B) cout << '<' << endl;
+    else if(A > B) cout << '>' << endl;
+    else if(A == B) cout << "==" << endl;
+    else cout << "잘못된 입력" << endl;
+    return 0;
+}
