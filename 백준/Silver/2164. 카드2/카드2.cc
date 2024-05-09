4774709f6f557d092a37ddd8c55a75810d38234c
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-05-10 01:34:04 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+	fast_io;
+	int n;
+	cin >> n;
+	queue<int> q;
+	for (int i = 0; i < n; i++) {
+		q.push(i + 1);
+	}
+	while (q.size() > 1) {
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+	cout << q.front();
+}
