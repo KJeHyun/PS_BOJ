aab2e0b479fb7d38479509a1e89c16071ada7d01
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-05-10 01:26:43 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+	fast_io;
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	sort(a.begin(), a.end());
+	cout << a[0] * a[n - 1];
+}
