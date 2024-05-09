46b9a45e95833141cfc85a398b86e3efb91983b2
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-05-10 01:40:34 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+    fast_io
+        string A, B;
+    cin >> A >> B;
+    reverse(A.begin(), A.end());
+    reverse(B.begin(), B.end());
+    if (A > B) { cout << A; }
+    else { cout << B; }
+}
