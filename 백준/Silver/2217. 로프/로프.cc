799007c978d82849d3db59ae729ee51b17832142
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-05-10 01:08:20 +0900

    [Silver IV] Title: 로프, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+	fast_io;
+	int n;
+	cin >> n;
+	vector<int> v;
+	int M = 0;
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < n; i++) {
+		M = max(M, v[i] * (n - i));
+	}
+	cout << M;
+}
