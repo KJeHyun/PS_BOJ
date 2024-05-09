4a5f0b982aead643469f0b7261a3741eee683da8
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-05-10 01:50:23 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+	fast_io
+		int A, B;
+	for (;;) {
+		cin >> A >> B;
+		if (A == 0 && B == 0) {
+			break;
+		}
+		cout << A + B << "\n";
+	}
+	return 0;
+}
