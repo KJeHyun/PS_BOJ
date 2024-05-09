f9f8922373956c1b033acf7b516bc67e63b20ec8
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-05-10 01:27:04 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <list>
+#include <set>
+#include <map>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+
+int main() {
+	fast_io;
+	int x,y,w,h;
+	cin >> x>>y>>w>>h;;
+	cout << min({ x, w - x, y, h - y });
+}
