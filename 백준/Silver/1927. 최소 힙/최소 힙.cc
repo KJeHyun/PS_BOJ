81a711c8767436fadd58fa7aec392e46b6b5df81
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-05-10 01:32:33 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+	fast_io;
+	int n;
+	cin >> n;
+	priority_queue<int, vector<int>, greater<int>> pq;
+	for (int i = 0; i < n; i++) {
+		int d;
+		cin >> d;
+		if (d == 0) {
+			if (!pq.empty()) {
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+			else cout << 0 << '\n';
+		}
+		else pq.push(d);
+	}
+}
