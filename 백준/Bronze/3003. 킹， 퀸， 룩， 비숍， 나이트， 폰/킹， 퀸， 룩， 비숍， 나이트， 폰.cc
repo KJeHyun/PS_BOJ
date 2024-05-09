a4ed218ed35bd742460fe7a0fc91bda80b17ddce
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-05-10 01:41:04 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+    fast_io
+        int a[6] = { 1,1,2,2,2,8 };
+    int b[6];
+    for (int i = 0; i < 6; i++) {
+        cin >> b[i];
+        cout << a[i] - b[i] << ' ';
+    }
+}
