40efcff5b347d66f55ca7543704dbcee9fb0d0b5
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-05-13 12:53:34 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+	fast_io;
+	int n;
+	cin >> n;
+	string s1, s2;
+	for (int i = 0; i <= 2*n; i++) {
+		s1.push_back((i % 2 == 0) ? '*' : ' ');
+		s2.push_back((i % 2 == 0) ? ' ' : '*');
+	}
+	s1.pop_back();
+	for (int i = 0; i < n; i++) {
+		if (i % 2 == 0) cout << s1;
+		else cout << s2;
+		cout << "\n";
+	}
+}
