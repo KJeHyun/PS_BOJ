9d1fdf9c74ba621e9dade9d830abb3a1e44cd46f
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-05-14 12:54:50 +0900

    [Silver III] Title: 01타일, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+ll dp[1'000'001];
+
+int main() {
+	fast_io;
+	int n;
+	cin >> n;
+	dp[1] = 1LL;
+	dp[2] = 2LL;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] % 15746 + dp[i - 2] % 15746;
+	}
+	cout << dp[n] % 15746;
+}
