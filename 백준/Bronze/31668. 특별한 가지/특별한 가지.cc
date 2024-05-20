5aa7d042df04f7cfe0e1362fa4bbb032e63f00c6
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-05-20 12:26:02 +0900

    [Bronze IV] Title: 특별한 가지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+    fast_io
+        int N, M, K;
+    cin >> N >> M >> K;
+    cout << M / N * K;
+}
