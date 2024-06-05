822f9af791223d82a76d303076f2e579a9cd1eb6
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-06-06 01:54:18 +0900

    [Bronze IV] Title: 모음의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+    fast_io;
+    while (1) {
+        string s;
+        getline(cin, s);
+        if (s == "#")
+            break;
+        int v = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
+                s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+                v++;
+        }
+        cout << v << '\n';
+    }
+}
