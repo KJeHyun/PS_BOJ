3023f98fdebb49400442048fed96f5a4384206d0
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-07-09 00:22:50 +0900

    [Silver V] Title: 아시아 정보올림피아드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+	fast_io;
+	int n;
+	cin >> n;
+	vector<pair<int,pii>> v(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> v[i].yy.xx >> v[i].yy.yy >> v[i].xx;
+	}
+	sort(v.begin(), v.end(), greater());
+	cout << v[0].yy.xx << " " << v[0].yy.yy << "\n"
+		<< v[1].yy.xx << " " << v[1].yy.yy << "\n";
+	if (v[0].yy.xx != v[1].yy.xx) {
+		cout << v[2].yy.xx << " " << v[2].yy.yy << "\n";
+	}
+	else {
+		for (int i = 2; i < n; ++i) {
+			if (v[i].yy.xx != v[0].yy.xx) {
+				cout << v[i].yy.xx << " " << v[i].yy.yy << "\n";
+				break;
+			}
+		}
+	}
+}
