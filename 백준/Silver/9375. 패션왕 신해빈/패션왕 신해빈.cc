3c954b1319cde5cc5be75363d69197ca1f2d4255
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-07-15 02:46:18 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+
+int main() {
+	fast_io;
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		unordered_map<string, int> um;
+		for (int i = 0; i < n; ++i) {
+			string x, y;
+			cin >> x >> y;
+			um[y]++;
+		}
+		int res = 1;
+		for (auto&[key,value]:um) {
+			res *= value + 1;
+		}
+		res--;
+		cout << res << "\n";
+	}
+}
