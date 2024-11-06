a83896607944e52645e6a74adb751db8537223c2
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-11-06 22:39:02 +0900

    [Bronze III] Title: 악마의 제안, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23972. 악마의 제안/악마의 제안.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+    fast_io;
+    ll k, n;
+    cin >> k >> n;
+    if (n == 1) cout << -1;
+    else cout << ((k * n) % (n - 1) ? k * n / (n - 1) + 1 : k * n / (n - 1));
+}
