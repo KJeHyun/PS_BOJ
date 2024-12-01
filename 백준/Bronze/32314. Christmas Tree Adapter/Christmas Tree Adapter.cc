a139b869f4b51cb887935a7a5805d5938a5c1412
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2024-12-02 00:40:23 +0900

    [Bronze V] Title: Christmas Tree Adapter, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32314. Christmas Tree Adapter/Christmas Tree Adapter.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+    fast_io;
+    int a, w, b;
+    cin >> a >> w >> b;
+    if (a <= w / b) cout << 1;
+    else cout << 0;
+}
