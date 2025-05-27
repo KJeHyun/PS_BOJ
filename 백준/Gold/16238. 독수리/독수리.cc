32379498797f1a8ac1224fde7453e050bf547193
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2025-05-27 22:24:40 +0900

    [Gold I] Title: 독수리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16238. 독수리/독수리.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+    fast_io;
+    int n;
+    cin >> n;
+    vector<int> A(n);
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    sort(A.begin(), A.end(), greater());
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        int temp = A[i] - i;
+        if (temp > 0) ans += temp;
+    }
+    cout << ans;
+}
