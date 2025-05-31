cefd64873777cb78f069027a9cd071618fe0dd4a
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2025-05-31 20:40:16 +0900

    [Bronze IV] Title: 등교, Time: 0 ms, Memory: 2024 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/31962. 등교/등교.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int main() {
+    fast_io;
+    int n, x;
+    cin >> n >> x;
+    vector<pii> v;
+    for (int i = 0; i < n; i++) {
+        int s, t;
+        cin >> s >> t;
+        if (s + t <= x) v.push_back({ s + t, s });
+    }
+    sort(v.begin(), v.end(), greater());
+    if (!v.empty()) cout << v[0].yy;
+    else cout << -1;
+}
