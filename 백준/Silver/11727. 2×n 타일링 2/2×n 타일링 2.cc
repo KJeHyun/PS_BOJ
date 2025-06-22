14b1b33605f8b8befa76ed8524e78334cdc495f4
KJeHyun <80564965+KJeHyun@users.noreply.github.com>
2025-06-22 18:36:37 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define fast_io cin.tie(NULL); ios_base::sync_with_stdio(false);
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef tuple<int, int, int> tiii;
+typedef tuple<ll, ll, ll> tlll;
+#define xx first
+#define yy second
+
+int dp[1001];
+
+int main() {
+    fast_io;
+    int n;
+    cin >> n;
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2] * 2;
+        dp[i] %= 10007;
+    }
+    cout << dp[n];
+}
